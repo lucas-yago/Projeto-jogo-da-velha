fce3ac051d7aebba298e57a97d5a4a4bc7f0b074
Lucas Yago <lucasyago63@gmail.com>
2026-03-30 16:12:07 -0300

    feat: implementa função para exibir tabuleiro

---
+++ src/main/java/JogoDaVelha.java
@@ -0,0 +1,25 @@
+public class JogoDaVelha {
+
+        private char[][] board;
+        private char currentPlayer;
+
+
+    public void showBoard() {
+
+        System.out.println( "      0     1     2   ");
+        System.out.println("   ───────────────────");
+        for (int i = 0; i < 3; i++) {
+            System.out.print(i + "  |  ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                System.out.print("  |  ");
+            }
+            System.out.println();
+            if (i < 2) System.out.println("   ┼─────┼─────┼─────┼");
+        }
+        System.out.println("   ───────────────────");
+
+    }
+
+}
+
